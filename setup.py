bbcc0723e03b0b340b2b3bde77992bea98acd41a
starkfire <plpantoja49@gmail.com>
2024-12-08 05:38:48 +0800

    chore: dynamically install dependencies and find packages

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+
+def get_requirements():
+    with open("requirements.txt") as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith("#")]
+
+requirements = get_requirements()
+
 setup(
     name="vcegen",
     version="0.1.0",
     description="Python library for generating VCE-ready files from PDFs",
     url="https://github.com/starkfire/vcegen",
-    packages=find_packages(exclude=('docs', 'tests'))
+    packages=find_packages(exclude=['demo', 'docs', 'tests']),
+    install_requires=requirements
 )
