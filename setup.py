206acb70aa9ea9d74f1e816a34045809f1ca4c20
starkfire <plpantoja49@gmail.com>
2024-12-07 15:27:45 +0800

    chore: update url reference on setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="vcegen",
     version="0.1.0",
-    description="Python CLI app for generating VCE-ready files from PDFs",
-    url="https://github.com/starkfire/vcegen-cli",
+    description="Python library for generating VCE-ready files from PDFs",
+    url="https://github.com/starkfire/vcegen",
     packages=find_packages(exclude=('docs', 'tests'))
 )
